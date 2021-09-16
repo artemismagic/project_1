ae13a21a32eca689be965f1c4dc2fbd22ce2fa8a
Kate Julia <katejuliaseattle@gmail.com>
2021-09-16 13:58:03 -0700

    update recipe card

--- js/get_recipe_card.js
+++ js/get_recipe_card.js
@@ -1,11 +1,9 @@
  //add html element id and classes here
- var food = document.getElementById('food');
- var fridge = document.getElementById('fridge');
- var queryList = [];
+ var getRecipeCard= document.getElementById('getCard');
  
  //function to add data to fridge list as well as the queryList, then submits request.  to do: populate recipe cards with results
      
- document.querySelector('#food').addEventListener('click', function (event) {
+ document.getElementById('#getCard').addEventListener('Click', function (event) {
     fetch('https://api.spoonacular.com/recipes/4632/card?query&apiKey=f5ee2e3ba0cc4a3abad3369a8d4f7db3').then(function(response){
          return(response.json())
          })
