7fcd6b1c6a0999de1cec374d166a137af9671a4f
Peter Kingsley <peterjkingsley@hotmail.com>
2021-09-18 12:55:44 -0700

    added to remove item function to update img list with current ingredients

--- js/search_recipes.js
+++ js/search_recipes.js
@@ -19,14 +19,28 @@ document.querySelector('#food').addEventListener('keypress', function (event) {
         removeButton.innerHTML = 'x';
         removeButton.addEventListener('click', function(event){
             event.preventDefault();
-            console.log(event.target);
-            console.log(event.target.parentElement.id);
             var removeIngredient = event.target.parentElement.id;
-            console.log(removeIngredient);
-            console.log(queryList.indexOf(removeIngredient));
             queryList.splice(removeIngredient, 1);
-            console.log(queryList);
             document.getElementById(removeIngredient).remove();
+            var request = fetch('https://api.spoonacular.com/recipes/findByIngredients?ingredients=' + queryList + '&number=9&apiKey=f5ee2e3ba0cc4a3abad3369a8d4f7db3').then(function(response){
+            return(response.json())
+            })
+            .then(function(data){
+                console.log(data);
+                document.querySelectorAll('.img').forEach(e => e.remove());
+                document.querySelectorAll('.title').forEach(e => e.remove());
+                for (i = 0; i < 9; i++){
+                    var page = document.createElement('img');
+                    var pageTitle = document.createElement('div');
+                    page.src = data[i].image;
+                    page.id = data[i].id;
+                    page.classList.add('img');
+                    pageTitle.classList.add('title');
+                    pageTitle.innerHTML = data[i].title;
+                    recipeBook.append(page);
+                    recipeBook.append(pageTitle);
+                };
+            });
         });
         ingredient.append(removeButton);
         queryList.push(food.value);
